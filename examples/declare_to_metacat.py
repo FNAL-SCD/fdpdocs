a485db7a29b66b2e28dd38c4382c896d52688be9
Alison Peisker <apeisker@fnal.gov>
2026-03-25 12:09:25 -0500

    updating dataset metadata logic

--- examples/declare_to_metacat.py
+++ examples/declare_to_metacat.py
@@ -24,25 +24,33 @@ def metacat_namespace(name):
 # create dataset if it does not already exist
 def metacat_dataset(name, ns, dsmdfile):
     # check if dataset already exists
-    did = ns+":"+name
+    did = f"{ns}:{name}"
     ds = client.get_dataset(did)
-    if ds is not None:
-        logger.info(f"Dataset {name} already exists, skipping creation")
-    else:
+    if ds is None:
         # create the dataset
         if dsmdfile is None:
             result = client.create_dataset(did)
         else:
+            # create the dataset with metadata from json file
             with open(dsmdfile, "r") as f:
                 dsmd = json.load(f)
             result = client.create_dataset(did, metadata=dsmd)
         logger.info(result)
+    else:
+        logger.info(f"Dataset {name} already exists, skipping creation")
+        # update the dataset metadata if given a dataset metadata file and the dataset already exists
+        if dsmdfile is not None:
+            with open(dsmdfile, "r") as f:
+                dsmd = json.load(f)
+            result = client.update_dataset(did, metadata=dsmd)
+            logger.info("Updated dataset metadata")
+            logger.info(result)
 
 # declare the files from the metadata file and add them to metacat; returns the number of files added
 def metacat_declare_add(dataset, batchfile, ns):
     with open(batchfile, "r") as f:
         files = json.load(f)
-    did = ns+":"+dataset
+    did = f"{ns}:{dataset}"
     result = client.declare_files(did, files, namespace=ns)
     result2 = client.add_files(did, files, namespace=ns)
     return len(files)
@@ -68,8 +76,11 @@ if __name__ == '__main__':
     metacat_dataset(dataset, namespace, dsmdfile)
 
     # add the files to metacat
-    if dsmdfile is not None:
+    if batchfile is not None:
         logging.info("Declaring and adding files to dataset")
         num_files = metacat_declare_add(dataset, batchfile, namespace)
         logging.info(f"Added {num_files} to dataset {dataset}")
+    else:
+        logging.info("No file metadata provided, skipping file declaration")
+
 
