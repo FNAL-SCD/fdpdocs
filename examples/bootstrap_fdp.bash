5625272183f45df55bc25e8ac4fcf3ef9834f077
Marc Mengel <mengel@fnal.gov>
2026-06-23 17:10:18 -0500

    adding amsc client stuff

--- examples/bootstrap_fdp.bash
+++ examples/bootstrap_fdp.bash
@@ -25,3 +25,10 @@ export METACAT_SERVER_URL=https://metacat.fnal.gov:9443/amsc_meta_prod/app
 export METACAT_AUTH_SERVER_URL=https://metacat.fnal.gov:8143/auth/amsc
 export UV_PYTHON_INSTALL_DIR=$PWD/python_versions
 EOF
+
+# add the amsc python client stuff
+git clone https://github.com/amsc-interfaces/amsc-client-tutorial.git
+
+(cd amsc-client-tutorial ; uv pip install -r requirements.txt)
+
+
